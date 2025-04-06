51f74b8e91bcf4396b8095b4934a5c897afc1cd0
Lucas Stopinski da Silva <160746304+LucasStop@users.noreply.github.com>
2025-04-06 19:26:18 -0300

    Remove import of Hash facade from BrandsController

--- app/Domain/Brands/Controllers/BrandsController.php
+++ app/Domain/Brands/Controllers/BrandsController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Hash;
 
 class BrandsController extends Controller
 {
