53606d2764ae40c2fd74cba4ea22ab1171d67f6d
LucasStop <lucas.stopinski@tecpuc.com.br>
2025-05-29 10:55:43 -0300

    refactor: reorganiza rotas de carros para separar rotas públicas e protegidas

--- app/Domain/Cars/routes/api.php
+++ app/Domain/Cars/routes/api.php
@@ -3,15 +3,23 @@
 use App\Domain\Cars\Controllers\CarsController;
 use Illuminate\Support\Facades\Route;
 
+// Rotas públicas (sem autenticação)
 Route::controller(CarsController::class)
     ->prefix('cars')
     ->group(function () {
         Route::get('/', 'index');
-        Route::post('/', 'store');
         Route::get('/{id}', 'show');
-        Route::put('/{id}', 'update');
-        Route::delete('/{id}', 'delete');
         Route::get('/model/{modelId}', 'byModel');
         Route::get('/status/{status}', 'byStatus');
         Route::get('/price-range', 'byPriceRange');
+    });
+
+// Rotas protegidas (requer autenticação)
+Route::controller(CarsController::class)
+    ->prefix('cars')
+    ->middleware('auth:api')
+    ->group(function () {
+        Route::post('/', 'store');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'delete');
     });
